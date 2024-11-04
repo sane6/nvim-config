400a31fb5a76d72a0d8775d2a799f707541fdd4d
Srdjan <srki.sanadrovic@gmail.com>
2024-11-04 21:25:45 +0100

    added tabstop settings

--- init.lua
+++ init.lua
@@ -104,6 +104,9 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
